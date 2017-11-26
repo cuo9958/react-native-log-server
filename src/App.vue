38999dbbdc1e75cc7dc23f17c691e17e0b8116b3
郭方超 <guofangchao@daling.com>
2017-11-26 23:41:09 +0800

    界面初步完成

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,94 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <div class="layout" id="app">
+    <div class="nav">
+      <div class="layout-logo">
+      </div>
+      
+    </div>
+    <div class="layout-content">
+      <Row type="flex" style="height:100%">
+        <Col span="5" class="menu">
+        <Menu active-name="1" theme="dark" width="auto" :open-names="['1']">
+          <MenuItem name="1">
+          <Icon type="ios-analytics"></Icon>
+          全局监控
+          </MenuItem>
+          <Submenu name="2">
+            <template slot="title">
+          <Icon type="ios-keypad">
+          </Icon>
+          用户管理
+</template>
+                            <MenuItem name="2-1">用户信息</MenuItem>
+                        </Submenu>
+                    </Menu>
+                </Col>
+                <Col span="19">
+                    <router-view/>
+                </Col>
+          </Row>
+        </div>
+        <div class="layout-copy">
+            2011-2016 &copy; TalkingData
+        </div>
+  </div>           
 </template>
 
 <script>
 export default {
-  name: 'app'
-}
+  name: "app"
+};
 </script>
 
 <style>
-html,body{
+html,
+body {
   margin: 0;
   padding: 0;
   background: #efefef;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.layout {
+  border: 1px solid #d7dde4;
+  background: #f5f7f9;
+}
+.nav {
+  height: 50px;
+  background: #1c2438;
+}
+.layout-logo {
+  width: 100px;
+  height: 30px;
+  background: #5b6270;
+  border-radius: 3px;
+  float: left;
+  position: relative;
+  top: 10px;
+  left: 20px;
+}
+.layout-content {
+  flex: 1;
+  min-height: 200px;
+  margin: 0;
+  overflow: hidden;
+  background: #fff;
+  position: relative;
+}
+.menu {
+  background: #495060;
+}
+.layout-content-main {
+  padding: 10px;
+}
+.layout-copy {
+  text-align: center;
+  padding: 5px 0;
+  color: #9ea7b4;
 }
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,28 +1,86 @@
 <template>
-<div>adwawd</div>           
+  <div class="main">
+    <div class="list-box">
+      <div class="box-title">
+        错误<Badge class="title-tip" :count="errCount"></Badge>
+      </div>
+      <Collapse accordion>
+        <Panel v-for="(item,index) of errList" :key="index">
+            史蒂夫·乔布斯
+            <p slot="content">史蒂夫·乔布斯（Steve Jobs），1955年2月24日生于美国加利福尼亚州旧金山，美国发明家、企业家、美国苹果公司联合创办人。</p>
+        </Panel>
+    </Collapse>
+    </div>
+    <div class="list-box">
+      <div class="box-title">
+        警告<Badge class="title-tip" :count="warmCount"></Badge>
+      </div>
+      <Collapse accordion>
+        <Panel v-for="(item,index) of warmList" :key="index">
+            史蒂夫·乔布斯
+            <p slot="content">史蒂夫·乔布斯（Steve Jobs），1955年2月24日生于美国加利福尼亚州旧金山，美国发明家、企业家、美国苹果公司联合创办人。</p>
+        </Panel>
+    </Collapse>
+    </div>
+    <div class="list-box">
+      <div class="box-title">
+        消息<Badge class="title-tip" :count="infoCount"></Badge>
+      </div>
+      <Collapse accordion>
+       <Panel v-for="(item,index) of infoList" :key="index">
+            史蒂夫·乔布斯
+            <p slot="content">史蒂夫·乔布斯（Steve Jobs），1955年2月24日生于美国加利福尼亚州旧金山，美国发明家、企业家、美国苹果公司联合创办人。</p>
+        </Panel>
+    </Collapse>
+    </div>
+
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "index",
-    data() {
-      return {
-        pwd: "",
-        nickname: "",
-      };
-    },
-    async mounted() {
-      
-      this.refresh()
-    },
-    methods: {
-      async refresh() {
-       
-      },
-    }
-  };
+import socketio from "socket.io-client";
+export default {
+  name: "index",
+  data() {
+    return {
+      errList: [],
+      errCount: 0,
+      warmList: [],
+      warmCount: 0,
+      infoList: [],
+      infoCount: 0
+    };
+  },
+  async mounted() {
+    // const socketuri=window.location.origin;
+    const socketuri = "http://120.78.57.59:3000";
+    var socket = socketio(socketuri, {
+      query: {
+        token: "client"
+      }
+    });
+    socket.on("connect", function() {});
+    this.refresh();
+  },
+  methods: {
+    async refresh() {}
+  }
+};
 </script>
 
 <style scoped>
-  
+.main {
+  display: flex;
+  padding: 10px;
+}
+.list-box {
+  flex: 1;
+  margin: 0 5px;
+}
+.box-title {
+  padding: 10px;
+}
+.title-tip {
+  float: right;
+}
 </style>
