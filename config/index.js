cd85c420c2e1eb76e8eac0236739d4567d4e9fcc
郭方超 <guofangchao@daling.com>
2017-11-28 14:10:10 +0800

    add

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     index: path.resolve(__dirname, '../views/index.html'),
     assetsRoot: path.resolve(__dirname, '../public'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '',
+    assetsPublicPath: './',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
@@ -30,7 +30,7 @@ module.exports = {
     port: process.env.PORT || 8080,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
     proxyTable: {
       "/api/":{target:"http://localhost:8090",changeOrigin: true}
     },
