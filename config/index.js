a7328eb3d33506fb861cecf504ab4869b2f28bd8
郭方超 <guofangchao@daling.com>
2017-11-28 14:13:11 +0800

    add

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     index: path.resolve(__dirname, '../views/index.html'),
     assetsRoot: path.resolve(__dirname, '../public'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/rnmonitor',
+    assetsPublicPath: '/rnmonitor/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
