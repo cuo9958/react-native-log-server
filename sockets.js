abd2a4768b9ac3afb041ec1be6453a9cc26f7cb0
郭方超 <guofangchao@daling.com>
2017-11-30 12:11:04 +0800

    add

--- sockets.js
+++ sockets.js
@@ -5,30 +5,30 @@ socketio.getServer = function (server) {
      io = socket_io(server);
     io.sockets.on("connection",function(socket){
         console.log("连接成功");
-        console.log(socket.handshake.query.token)
+        // console.log(socket.handshake.query.token)
         socket.on("log",function(obj){
-            io.emit("log",obj);
+            io.broadcast.emit("log",obj);
         })
     });
 }
 socketio.emitMsg=function(msg){
     if(io){
-        io.emit("log",msg);
+        io.sockets.emit("log",msg);
     }
 }
 socketio.emitInfo=function(msg){
     if(io){
-        io.emit("info",msg);
+        io.sockets.emit("info",msg);
     }
 }
 socketio.emitWarm=function(msg){
     if(io){
-        io.emit("warm",msg);
+        io.sockets.emit("warm",msg);
     }
 }
 socketio.emitErr=function(msg){
     if(io){
-        io.emit("err",msg);
+        io.sockets.emit("err",msg);
     }
 }
 module.exports = socketio; 
