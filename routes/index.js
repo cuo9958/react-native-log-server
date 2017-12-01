3757fd1cedc34735cbf1f9246c9860a0561c9b6c
郭方超 <guofangchao@daling.com>
2017-12-01 10:46:34 +0800

    add

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,8 @@ nameList.add("dev")
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  console.log(`访问来源地址：${req.headers.host},${req.hostname},${req.url};上一个页面地址：${req.headers.referer}`)
   if(!req.headers.host)return res.end('');
-  console.log(`访问来源地址：${req.headers.host},${req.url};上一个页面地址：${req.headers.referer}`)
   res.render('index', {});
 });
 router.get('/index', function (req, res, next) {
